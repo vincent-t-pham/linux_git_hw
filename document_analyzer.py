24210356ab5e86e1ca34dee933527d774f45df51
Vincent Pham <vincent.t.pham@sjsu.edu>
2022-03-10 23:07:21 -0800

    Added document_analyzer.py

---
+++ document_analyzer.py
@@ -0,0 +1,42 @@
+string = """A few months ago, I decided to deaccession an assortment of my things by 
+whatever means feasible: selling, donating, recycling, giving them away, 
+losing them on the subway, or reserving a spot for them on the next Mars 
+Explorer. I gathered my unwanteds and piled them in the living room. A 
+fraction of what was in that jumble: seven antique glass cake stands that 
+belonged to my mother; a dormitoryâ€™s worth of new sheet sets and 
+blankets for a bed size that is not mine; a set of Lenox china that my 
+grandmother gave to my mother, who gave it to me, and was never used; 
+clothes galore; a Viking stove grate that arrived cracked, and which I 
+saved because I planned to weld it into a sculpture someday, after I 
+learned how to weld; several rolls of Trump toilet paper that I wrongly 
+thought were amusing a few years ago. I wish I could have added my 
+boyfriendâ€™s too large Le Corbusier lounger. (There are Web sites, such 
+as NeverLikedItAnyway.com, that will buy your exâ€™s leavings, ranging 
+from engagement rings to â€œRick and Mortyâ€ socks.)"""
+
+count = {}
+words = string.split()
+
+for word in words:
+	if word in count:
+		count[word] += 1
+	else:
+		count[word] = 1
+		
+i = 0
+frequent_key = []
+frequent_value = []
+for freq in sorted(count, key=count.get, reverse=True):
+	if(i<5):
+		frequent_key.append(freq)
+		i+=1
+
+for key in frequent_key: 
+	frequent_value.append(count[key])
+
+for i in range(5):
+	print(f'{frequent_key[i]}: {frequent_value[i]}')
+
+
+
+
